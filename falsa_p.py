cd5562d82e067268fe5af392db01ee58d7808661
emorenoemb <emoreno.emb@gmail.com>
2021-03-18 21:57:17 -0600

    Create falsa_p.py

---
+++ falsa_p.py
@@ -0,0 +1,54 @@
+#importar librerias
+import matplotlib.pyplot as plt
+import math
+import numpy as np
+#definicion de la funcion y del metodo de biseccion
+def fun(x):
+    return math.exp(-x)-x
+def falsa(xi,xif,xf,xff):
+    return (xi-(xif*1.0*(xf-xi))/(xff-xif))
+#valores contantes limite iniciao, limite final y numero de puntos de la grafica
+a,b,p=-2,5,100
+#valor verdadero a considerar
+vv=0.567143
+#variables donde se guardara los errores del metodo
+exb=[]
+eyb=[]
+vpm=[]
+#calculo del dominio y codominio e la funcion
+x = np.linspace(a,b,p)
+y = [fun(i) for i in x]
+#variables auxiliares
+error=100
+i=0
+while error>.05:
+    #metodo de biseccion
+    pm=falsa(a,fun(a),b,fun(b))
+    exb.append(i+1)
+    vpm.append(pm)
+    error=abs((vv-pm)*100/vv)
+    eyb.append(error)
+    print (pm,error)
+    i=i+1
+    #aplicacion del teorea de Bolzano y eleccion de subintervalo
+    if (fun(a)*fun(pm)<0):
+        b=pm
+    else:
+        a=pm
+#presentación de resultaods
+plt.figure(figsize=(9, 3))
+plt.subplot(131)
+plt.xlabel("x")
+plt.ylabel("f(x)")
+plt.plot(x,y,label=r'$e^{-x}-x$')
+plt.legend()
+plt.subplot(132)
+plt.xlabel("iteración")
+plt.ylabel("Raíz")
+plt.plot(exb,vpm,'-o')
+plt.subplot(133)
+plt.xlabel("iteración")
+plt.ylabel("erpv")
+plt.semilogy(exb,eyb,'-o')
+plt.suptitle('Falsa')
+plt.show()
